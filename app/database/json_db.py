fcad0954b4f154425ce925268bacee3862bd24bb
John Lundgren <john.lund.af@gmail.com>
2026-05-07 18:52:25 +0200

    class JsonDb for json interaction

---
+++ app/database/json_db.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+#from app.database.base import DBInterface
+
+class JsonDB:
+
+    def __init__(self, file_path: str = "/app/db/contacts.json"):
+        base_dir = Path(__file__).resolve().parent.parent
+        self.file_path = base_dir / "db" / "contacts.json"
+
+    def load(self) -> dict:
+        with open(self.file_path) as file:
+            return json.load(file)
+
+    def save(self, data: dict) -> None:
+        with self.file_path.open("w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4)
+

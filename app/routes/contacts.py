86b9b1b67b433e73f0beea5746ce86f50a9610bf
John Lundgren <john.lund.af@gmail.com>
2026-05-20 06:20:02 +0200

    Added UUID as type for contact id's

--- app/routes/contacts.py
+++ app/routes/contacts.py
@@ -1,3 +1,4 @@
+from uuid import UUID
 from fastapi import APIRouter, HTTPException, Query
 from app.database.json_db import JsonDB
 from app.models.contact_models import Contact, ContactCreate
@@ -27,11 +28,11 @@ def search_contacts(query: str = Query(..., min_lenght=1)):
 
 
 @router.get("/{contact_id}", response_model=Contact)
-def get_contact(contact_id):
+def get_contact(contact_id: UUID):
     try:
         return contact_repo.get_contact(contact_id)
-    except ContactNotFoundError as cfexc:
-        raise HTTPException(status_code=404, detail=str(cfexc))
+    except ContactNotFoundError as cnfexc:
+        raise HTTPException(status_code=404, detail=str(cnfexc))
     except Exception as exc:
         print(exc)
         raise HTTPException(status_code=500, detail="Internal error fetching contact")
@@ -47,7 +48,7 @@ def create_contact(contact: ContactCreate):
 
 
 @router.delete("/{contact_id}")
-def delete_contact(contact_id: str):
+def delete_contact(contact_id: UUID):
     try:
         return contact_repo.remove_contact(contact_id)
     except ContactNotFoundError as exc:
@@ -61,39 +62,3 @@ def update_contact(contact_id, updated_contact: dict):
     except ContactNotFoundError as exc:
         raise HTTPException(status_code=404, detail=str(exc))
 
-
-
-
-
-#
-# @router.put("/{contact_id}", response_model=Contact)
-# def update_contact(contact_id: UUID, updated_data: ContactUpdate):
-#     contact = contacts_db.get(contact_id)
-#
-#     if contact is None:
-#         raise HTTPException(status_code=404, detail="Contact not found")
-#
-#     update_dict = updated_data.model_dump(exclude_unset=True)
-#
-#     contact.update(update_dict)
-#     contact["updated_at"] = datetime.now()
-#
-#     contacts_db[contact_id] = contact
-#     return contact
-#
-# @router.post("/", response_model=UserResponse, status_code=201)
-# def create_user(user: UserCreate):
-#     try:
-#         return user_repository.create_user(user.model_dump())
-#     except UserAlreadyExistsError as exc:
-#         raise HTTPException(status_code=409, detail=str(exc))
-#
-#
-# @router.delete("/{user_id}", response_model=UserResponse | None)
-# def del_user(user_id: str):
-#     try:
-#         return user_repository.remove_user(user_id)
-#     except UserNotFoundError as exc:
-#         raise HTTPException(status_code=404, detail=str(exc))
-#
-#
